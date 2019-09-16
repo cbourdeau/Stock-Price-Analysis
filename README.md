ccc17fd6088b70a2fdd7cad294fbbbb9bb3ca631
Christian Bourdeau <50560300+cbourdeau@users.noreply.github.com>
2019-09-16 11:56:50 -0700

    updated README

--- README.md
+++ README.md
@@ -1,81 +1,32 @@
-# The VBA of Wall Street
+# Stock Analysis using VBA
 
-## Background
+![Stock Image](Images/stockmarket.jpg)
 
-You are well on your way to becoming a programmer and Excel master! In this homework assignment you will use VBA scripting to analyze real stock market data. Depending on your comfort level with VBA, choose your assignment from Easy, Moderate, or Hard below.
+## Business Requirements
+
+1. Calculate the total volume each stock had over each year
+2. Calculate the yearly dollar and percent change from the stock open price at the beginning of the year vs the closing price and the end of the year
+3. Create conditional formatting that will highlight increases in stock price in green and decreases in red
+4. Determine the stock with the "Greatest % Increase", "Greatest % Decrease" and "Greatest Total Volume"
+5. Allow this macro to be run on every worksheet by running it only once
 
 ### Files
 
-* [Test Data](Resources/alphabtical_testing.xlsx) - Use this while developing your scripts.
+* [Stock Data Raw](Raw_Data/stock_data_raw.xlsx) - Run your scripts on this data to generate the final homework report.
+* [Stock Data w/VBA](Raw_Data/stock_data_wVBA.xlsm) - Run your scripts on this data to generate the final homework report.
 
-* [Stock Data](Resources/Multiple_year_stock_data.xlsx) - Run your scripts on this data to generate the final homework report.
+### Results
 
-### Stock market analyst
+![Part 1](Images/part1.png)
 
-![stock Market](Images/stockmarket.jpg)
+![Part 2](Images/part2.png)
 
-### Easy
+![Part 3](Images/part3.png)
 
-* Create a script that will loop through one year of stock data for each run and return the total volume each stock had over that year.
+### Key Technical Skills Applied
 
-* You will also need to display the ticker symbol to coincide with the total stock volume.
-
-* Your result should look as follows (note: all solution images are for 2015 data).
-
-![easy_solution](Images/easy_solution.png)
-
-### Moderate
-
-* Create a script that will loop through all the stocks for one year for each run and take the following information.
-
-  * The ticker symbol.
-
-  * Yearly change from opening price at the beginning of a given year to the closing price at the end of that year.
-
-  * The percent change from opening price at the beginning of a given year to the closing price at the end of that year.
-
-  * The total stock volume of the stock.
-
-* You should also have conditional formatting that will highlight positive change in green and negative change in red.
-
-* The result should look as follows.
-
-![moderate_solution](Images/moderate_solution.png)
-
-### Hard
-
-* Your solution will include everything from the moderate challenge.
-
-* Your solution will also be able to return the stock with the "Greatest % increase", "Greatest % Decrease" and "Greatest total volume".
-
-* Solution will look as follows.
-
-![hard_solution](Images/hard_solution.png)
-
-### CHALLENGE
-
-* Make the appropriate adjustments to your script that will allow it to run on every worksheet, i.e., every year, just by running it once.
-
-* This can be applied to any of the difficulties.
-
-### Other Considerations
-
-* Use the sheet `alphabetical_testing.xlsx` while developing your code. This data set is smaller and will allow you to test faster. Your code should run on this file in less than 3-5 minutes.
-
-* Make sure that the script acts the same on each sheet. The joy of VBA is to take the tediousness out of repetitive task and run over and over again with a click of the button.
-
-## Submission
-
-* To submit please upload the following to Github:
-
-  * A screen shot for each year of your results on the Multi Year Stock Data.
-
-  * VBA Scripts as separate files.
-
-* After everything has been saved, create a sharable link and submit that to <https://bootcampspot-v2.com/>.
-
-- - -
-
-### Copyright
-
-Coding Boot Camp © 2019. All Rights Reserved.
+* Excel
+* Visual Basic
+* Macros
+* Conditional Statements (if, elseif, else, then)
+* Loops (For loops)
---
+++ Stock Ticker Analysis.vb
@@ -0,0 +1,180 @@
+Sub stock_analyzer():
+    ' Iterate through each worksheet
+    For Each ws In ActiveWorkbook.Worksheets
+
+    ' Declare variables
+    Dim lastRow As Double
+    Dim ticker As String
+    Dim Volume As Variant
+    Dim yearOpen As Double
+    Dim yearClose As Double
+    Dim yearChange As Single
+    Dim percentChange As Variant
+    Dim rowCounter As Variant
+    Dim resultsCounter As Variant
+    
+    ' Labels for the analysis fields
+    ws.Cells(1, 9).Value = "Ticker"
+    ws.Cells(1, 10).Value = "Yearly Change"
+    ws.Cells(1, 11).Value = "Percent Change"
+    ws.Cells(1, 12).Value = "Total Stock volume"
+    ws.Cells(1, 16).Value = "Ticker"
+    ws.Cells(1, 17).Value = "Value"
+    ws.Cells(2, 15).Value = "Greatest % Increase"
+    ws.Cells(3, 15).Value = "Greatest % Decrease"
+    ws.Cells(4, 15).Value = "Greatest Total Volume"
+
+    ' Results table starts at the second row
+    resultsCounter = 2
+
+    ' Counts the up to the last row
+    lastRow = ws.Cells(Rows.Count, 1).End(xlUp).Row
+    
+    'ws.Cells(2, 13).Value = lastRow
+    Volume = 0
+
+    ' Capture the opening year value
+    yearOpen = ws.Cells(2, 3).Value
+    
+    ' For loop to iterate through the second row to the last
+    For rowCounter = 2 To lastRow
+        
+        'Sums the total volume
+        Volume = Volume + ws.Cells(rowCounter, 7).Value
+
+'-----PART 1------
+
+        ' This part finds the percentChange, yearChange and conditionally formats the values under yearChange
+        
+        ' If the current ticker is the same as the one above and the ticker below is different than the current ticker then.
+        If (ws.Cells(rowCounter - 1, 1).Value = ws.Cells(rowCounter, 1).Value And ws.Cells(rowCounter + 1, 1).Value <> ws.Cells(rowCounter, 1).Value) Then
+            
+            ' Capture the year close value at the current row
+            yearClose = ws.Cells(rowCounter, 6).Value
+
+            ' Calculate the change between close and open value
+            yearChange = yearClose - yearOpen
+
+            ' The if statement below handles a division by zero, the result can be changed to NaN stirng
+            If yearOpen = 0 And yearClose <> 0 Then
+                percentChange = yearClose / yearClose
+            ElseIf yearOpen = 0 And yearClose = 0 Then
+                percentChange = 0
+            Else
+                ' We calculate percent change, normally we multiply by 100 but we will add % and * 100 when displaying the value
+                percentChange = (yearClose - yearOpen) / yearOpen
+            End If
+
+            ' We add one so that we move to the next year open
+            yearOpen = ws.Cells(rowCounter + 1, 3).Value
+            
+            ' Display year change by ticker
+            ws.Cells(resultsCounter, 10).Value = yearChange
+            ws.Cells(resultsCounter, 10).NumberFormat = "#.00;(#.00)"
+
+            ' Conditional formating for year change: positive green, negative red
+            If ws.Cells(resultsCounter, 10).Value < 0 Then
+
+                ' Less than zero will be red
+                ws.Cells(resultsCounter, 10).Interior.ColorIndex = 3
+            Else
+
+                ' Positive results and zero will be green
+                ws.Cells(resultsCounter, 10).Interior.ColorIndex = 4
+            
+        End If
+            
+            ' Display percent change by ticker
+            ws.Cells(resultsCounter, 11).Value = percentChange
+            
+            ' Format the numbers as a percentage with % and two decimal places
+            ws.Cells(resultsCounter, 11).NumberFormat = "0.00%"
+            
+        End If
+
+'-----PART 2------
+
+        'This section finds the total volume per ticker
+
+        ' To display the total volume per ticker we check if the cell below is different
+        If (ws.Cells(rowCounter + 1, 1).Value <> ws.Cells(rowCounter, 1).Value) Then
+            
+            ' Display tickers
+            ws.Cells(resultsCounter, 9).Value = ws.Cells(rowCounter, 1).Value
+            
+            ' Print total volume in column 12
+            ws.Cells(resultsCounter, 12).Value = Volume
+            ws.Cells(resultsCounter, 12).NumberFormat = "#,###"
+
+            ' We reset the volume for the next ticker
+            Volume = 0
+
+           ' We increase the results counter by one for the next loop
+           resultsCounter = resultsCounter + 1
+
+        End If
+       
+    Next rowCounter
+    
+'-----PART 3------
+
+    ' Last part displays the final results section showcasing the greatest increase, greatest decrease, and greatest volume comparison
+
+    ' Initializing values for our loop to compare against
+    ws.Cells(2, 17).Value = 0
+    ws.Cells(3, 17).Value = 0
+    ws.Cells(4, 17).Value = 0
+
+    ' Start loop for the final results
+    For resultsCounter = 2 To lastRow
+
+        ' If the percent change is greater than the temporary value stored in cell (2,17)
+        If ws.Cells(resultsCounter, 11).Value > ws.Cells(2, 17).Value Then
+
+            ' Store the new temporary greater percent change value in cell (2,17)
+            ws.Cells(2, 17).Value = ws.Cells(resultsCounter, 11).Value
+
+            ' This statement will find the greatest value and print it to the results table
+            ws.Cells(2, 16).Value = ws.Cells(resultsCounter, 9).Value
+            
+        End If
+
+        ' If the percent change is lower than the temporary value stored in cell (3,17)
+        If ws.Cells(resultsCounter, 11).Value < ws.Cells(3, 17).Value Then
+
+            ' Store the new temporaty percent change value in cell (3,17)
+            ws.Cells(3, 17).Value = ws.Cells(resultsCounter, 11).Value
+
+            ' This statement will find the lowest value and print it the results table
+            ws.Cells(3, 16).Value = ws.Cells(resultsCounter, 9).Value
+        
+        End If
+
+        ' If the volume is greater than the temporary value in cell (4, 17)
+         If ws.Cells(resultsCounter, 12).Value > ws.Cells(4, 17).Value Then
+
+            ' Store the greater volume in cell (4,17)
+            ws.Cells(4, 17).Value = ws.Cells(resultsCounter, 12).Value
+
+            ' This statement will find the highest volume and print it in the results table
+            ws.Cells(4, 16).Value = ws.Cells(resultsCounter, 9).Value
+            
+        End If
+       
+        Next resultsCounter
+        
+        'Format the results table
+        ws.Cells(2, 17).NumberFormat = "0.00%"
+        ws.Cells(3, 17).NumberFormat = "0.00%"
+        ws.Cells(4, 17).NumberFormat = "#,###"
+       
+       'Autosize columns to make the numbers fit
+        ws.Columns(10).AutoFit
+        ws.Columns(11).AutoFit
+        ws.Columns(12).AutoFit
+        ws.Columns(15).AutoFit
+        ws.Columns(17).AutoFit
+    
+    Next ws
+ 
+End Sub
